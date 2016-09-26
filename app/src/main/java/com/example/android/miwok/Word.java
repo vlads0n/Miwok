f6849fad44cc6646f14dadef7a4265ddb38a6178
vlad_son <19vlad_son90@bk.ru>
2016-09-26 15:04:19 +0300

    Miwok

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -6,18 +6,21 @@ package com.example.android.miwok;
 public class Word {
     private String defaultText;
     private String miwokText;
+    private int audio;
     private int image = NO_IMAGE;
     private static final int NO_IMAGE = -1;
 
-    public Word(String defaultText, String miwokText) {
+    public Word(String defaultText, String miwokText, int audio) {
         this.defaultText = defaultText;
         this.miwokText = miwokText;
+        this.audio = audio;
     }
 
-    public Word(String defaultText, String miwokText, int image) {
+    public Word(String defaultText, String miwokText, int image, int audio) {
         this.defaultText = defaultText;
         this.miwokText = miwokText;
         this.image = image;
+        this.audio = audio;
     }
 
     public String getDefaultText() {
@@ -30,6 +33,10 @@ public class Word {
 
     public int getImage() {
         return image;
+    }
+
+    public int getAudio() {
+        return audio;
     }
 
     public boolean hasImage() {
